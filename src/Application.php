f1a041c1da6bf13eb9d3f3f6c9b09f977f131e77
giumar <info@giumar.net>
2022-10-12 04:48:23 +0200

    Autenticazione e gesione degli utenti.

--- src/Application.php
+++ src/Application.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 /**
@@ -14,6 +15,7 @@ declare(strict_types=1);
  * @since     3.3.0
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App;
 
 use Cake\Core\Configure;
@@ -27,6 +29,14 @@ use Cake\Http\MiddlewareQueue;
 use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
+//CakePHP Authentication Middleware
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Identifier\IdentifierInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Application setup class.
@@ -34,15 +44,14 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
  */
-class Application extends BaseApplication
-{
+class Application extends BaseApplication implements AuthenticationServiceProviderInterface {
+
     /**
      * Load all the application configuration and bootstrap logic.
      *
      * @return void
      */
-    public function bootstrap(): void
-    {
+    public function bootstrap(): void {
         // Call parent to load bootstrap from files.
         parent::bootstrap();
 
@@ -50,8 +59,8 @@ class Application extends BaseApplication
             $this->bootstrapCli();
         } else {
             FactoryLocator::add(
-                'Table',
-                (new TableLocator())->allowFallbackClass(false)
+                    'Table',
+                    (new TableLocator())->allowFallbackClass(false)
             );
         }
 
@@ -64,6 +73,7 @@ class Application extends BaseApplication
         }
 
         // Load more plugins here
+        $this->addPlugin('Authentication');
     }
 
     /**
@@ -72,36 +82,36 @@ class Application extends BaseApplication
      * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
      * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
      */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue {
         $middlewareQueue
-            // Catch any exceptions in the lower layers,
-            // and make an error page/response
-            ->add(new ErrorHandlerMiddleware(Configure::read('Error')))
+                // Catch any exceptions in the lower layers,
+                // and make an error page/response
+                ->add(new ErrorHandlerMiddleware(Configure::read('Error')))
 
-            // Handle plugin/theme assets like CakePHP normally does.
-            ->add(new AssetMiddleware([
-                'cacheTime' => Configure::read('Asset.cacheTime'),
-            ]))
+                // Handle plugin/theme assets like CakePHP normally does.
+                ->add(new AssetMiddleware([
+                            'cacheTime' => Configure::read('Asset.cacheTime'),
+                        ]))
 
-            // Add routing middleware.
-            // If you have a large number of routes connected, turning on routes
-            // caching in production could improve performance. For that when
-            // creating the middleware instance specify the cache config name by
-            // using it's second constructor argument:
-            // `new RoutingMiddleware($this, '_cake_routes_')`
-            ->add(new RoutingMiddleware($this))
+                // Add routing middleware.
+                // If you have a large number of routes connected, turning on routes
+                // caching in production could improve performance. For that when
+                // creating the middleware instance specify the cache config name by
+                // using it's second constructor argument:
+                // `new RoutingMiddleware($this, '_cake_routes_')`
+                ->add(new RoutingMiddleware($this))
 
-            // Parse various types of encoded request bodies so that they are
-            // available as array through $request->getData()
-            // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
-            ->add(new BodyParserMiddleware())
+                // Parse various types of encoded request bodies so that they are
+                // available as array through $request->getData()
+                // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
+                ->add(new BodyParserMiddleware())
 
-            // Cross Site Request Forgery (CSRF) Protection Middleware
-            // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
-            ->add(new CsrfProtectionMiddleware([
-                'httponly' => true,
-            ]));
+                // Cross Site Request Forgery (CSRF) Protection Middleware
+                // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
+                ->add(new CsrfProtectionMiddleware([
+                            'httponly' => true,
+                        ]))
+                ->add(new AuthenticationMiddleware($this));
 
         return $middlewareQueue;
     }
@@ -113,8 +123,8 @@ class Application extends BaseApplication
      * @return void
      * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
      */
-    public function services(ContainerInterface $container): void
-    {
+    public function services(ContainerInterface $container): void {
+        
     }
 
     /**
@@ -124,8 +134,7 @@ class Application extends BaseApplication
      *
      * @return void
      */
-    protected function bootstrapCli(): void
-    {
+    protected function bootstrapCli(): void {
         $this->addOptionalPlugin('Cake/Repl');
         $this->addOptionalPlugin('Bake');
 
@@ -133,4 +142,47 @@ class Application extends BaseApplication
 
         // Load more plugins here
     }
+
+    /**
+     * Returns a service provider instance.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request Request
+     * @return \Authentication\AuthenticationServiceInterface
+     */
+    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface {
+        $service = new AuthenticationService();
+
+        // Define where users should be redirected to when they are not authenticated
+        $service->setConfig([
+            'unauthenticatedRedirect' => Router::url([
+                'prefix' => false,
+                'plugin' => null,
+                'controller' => 'Users',
+                'action' => 'login',
+            ]),
+            'queryParam' => 'redirect',
+        ]);
+
+        $fields = [
+            IdentifierInterface::CREDENTIAL_USERNAME => 'email',
+            IdentifierInterface::CREDENTIAL_PASSWORD => 'password'
+        ];
+        // Load the authenticators. Session should be first.
+        $service->loadAuthenticator('Authentication.Session');
+        $service->loadAuthenticator('Authentication.Form', [
+            'fields' => $fields,
+            'loginUrl' => Router::url([
+                'prefix' => false,
+                'plugin' => null,
+                'controller' => 'Users',
+                'action' => 'login',
+            ]),
+        ]);
+
+        // Load identifiers
+        $service->loadIdentifier('Authentication.Password', compact('fields'));
+
+        return $service;
+    }
+
 }
