bcb877c01f203f0ba406ada1d9167545d7182e61
giumar <info@giumar.net>
2022-10-13 19:53:32 +0200

    Ridimensionamento elementi <header> e <footer> del template.

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -25,62 +25,54 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <?= $this->fetch('title') ?>
         </title>
         <?= $this->Html->meta('icon') ?>
-        
+
         <?= $this->Html->css(['bootstrap.min']) ?>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-        
+
         <?= $this->fetch('meta') ?>
         <?= $this->fetch('css') ?>
         <?= $this->fetch('script') ?>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="<?= $this->Url->build('/'); ?>"><i class="bi bi-house-fill"></i> Home</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/events'); ?>"><i class="bi bi-calendar-event"></i> Eventi</a>
-                        </li>
-                        <li class="nav-item">
-                            <?php if($this->request->getSession()->read('Auth.id')) { ?>
-                                <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/logout'); ?>"><i class="bi bi-door-closed-fill"></i>Logout</a>
-                            <?php } else { ?>
-                                <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/login'); ?>"><i class="bi bi-door-open-fill"></i>Login</a>
-                            <?php } ?>                            
-                        </li>                        
-                    </ul>
-                    <?php if($this->request->getSession()->read('Auth.id')) : ?>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <?php endif; ?>
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/events'); ?>"><i class="bi bi-calendar-event"></i> Eventi</a>
-                        </li>
-                        <li class="nav-item">
-                            <?php if($this->request->getSession()->read('Auth.id')) { ?>
-                                <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/logout'); ?>"><i class="bi bi-door-closed-fill"></i>Logout</a>
-                            <?php } else { ?>
-                                <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/login'); ?>"><i class="bi bi-door-open-fill"></i>Login</a>
-                            <?php } ?>                            
-                        </li>                        
-                    </ul>
+        <header class="container">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="<?= $this->Url->build('/'); ?>"><i class="bi bi-house-fill"></i> Home</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/events'); ?>"><i class="bi bi-calendar-event"></i> Eventi</a>
+                            </li>                     
+                        </ul>
+                        <?php if ($this->request->getSession()->read('Auth.id')) : ?>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        <?php endif; ?>
+                        <ul class="navbar-nav justify-content-end">
+                            <li class="nav-item">
+                                <?php if ($this->request->getSession()->read('Auth.id')) { ?>
+                                    <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/logout'); ?>"><i class="bi bi-door-closed-fill"></i>Logout</a>
+                                <?php } else { ?>
+                                    <a class="nav-link" aria-current="page" href="<?= $this->Url->build('/users/login'); ?>"><i class="bi bi-door-open-fill"></i>Login</a>
+                                <?php } ?>                            
+                            </li>                        
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
         <main class="main">
             <div class="container">
                 <?= $this->Flash->render() ?>
                 <?= $this->fetch('content') ?>
             </div>
         </main>
-        <footer>
+        <footer class="container">
         </footer>
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <script src="js/bootstrap.min.js"></script>
