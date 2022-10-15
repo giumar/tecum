546c63d9bd39c082b94f660053057ffcd0a2154c
giumar <info@giumar.net>
2022-10-15 07:09:53 +0200

    Rimossi i test per i metodi inesistenti del Controller Home. Close #26

--- tests/TestCase/Controller/HomeControllerTest.php
+++ tests/TestCase/Controller/HomeControllerTest.php
@@ -22,7 +22,7 @@ class HomeControllerTest extends TestCase
      * @var array<string>
      */
     protected $fixtures = [
-        'app.Home',
+        //'app.Home',
     ];
 
     /**
@@ -32,50 +32,6 @@ class HomeControllerTest extends TestCase
      * @uses \App\Controller\HomeController::index()
      */
     public function testIndex(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test view method
-     *
-     * @return void
-     * @uses \App\Controller\HomeController::view()
-     */
-    public function testView(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test add method
-     *
-     * @return void
-     * @uses \App\Controller\HomeController::add()
-     */
-    public function testAdd(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test edit method
-     *
-     * @return void
-     * @uses \App\Controller\HomeController::edit()
-     */
-    public function testEdit(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test delete method
-     *
-     * @return void
-     * @uses \App\Controller\HomeController::delete()
-     */
-    public function testDelete(): void
     {
         $this->markTestIncomplete('Not implemented yet.');
     }
