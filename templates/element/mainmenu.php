7d52c488192fb45b7b952f8dd36f757aa4b2004f
giumar <info@giumar.net>
2023-04-22 07:55:13 +0200

    Creazione dell'area profilo utente

--- templates/element/mainmenu.php
+++ templates/element/mainmenu.php
@@ -40,6 +40,11 @@
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
+                <ul class="navbar-nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="<?= $this->Url->build(['prefix' => 'User', 'controller' => 'Profile', 'action' => 'index']); ?>">Profile</a>
+                    </li>
+                </ul>
             <?php endif; ?>
             <ul class="navbar-nav justify-content-end">
                 <li class="nav-item">
